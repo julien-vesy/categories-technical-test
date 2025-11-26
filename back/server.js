da6f81ec12abc01b914d3794f3e2a30f49409dfe
Julien Vesy <julien.vesy@smile.fr>
2025-11-26 10:37:24 +0100

    mouving

---
+++ back/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+app.use(cors());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Charger le JSON
+const dataPath = path.join(__dirname, "data", "categories.json");
+const rawData = fs.readFileSync(dataPath);
+const db = JSON.parse(rawData);
+
+// Route : /all-categories
+app.get("/all-categories", (req, res) => {
+    res.json(db.categories);
+});
+
+// Route : /visible-categories
+app.get("/visible-categories", (req, res) => {
+    res.json(db.visibleCategories);
+});
+
+// Lancement serveur
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`API running at http://localhost:${PORT}`);
+});
